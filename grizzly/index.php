31edc068abd2c63b38baeb39a4ebed36331b474f
danuambar22 <danuambartumean22@gmail.com>
2024-08-27 01:25:32 +0300

    Add files via upload

--- grizzly/index.php
+++ grizzly/index.php
@@ -1,5 +1,15 @@
 <?php
+$host = "c9mq4861d16jlm.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com";
+$port = "5432";
+$dbname = "d28r8jq3pdg6b5";
+$username = "ufpmtng6gkep53";
+$password = "p024bcec6f727edeb8e1dbcdbf1dc0c03bad91664d8fe944dfc196a3bca8fb53f";
 
-include "menu.html";
-include "home.html";
-include "contacts.html";
+try {
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require";
+    $pdo = new PDO($dsn, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully!";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
